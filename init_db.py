04d1ca4a483c642380d4ec67113c072e29483f4d
Yuanming Zhang <ymzhang.cs@gmail.com>
2025-04-30 17:35:21 +0800

    fix db init

--- init_db.py
+++ init_db.py
@@ -1,12 +1,7 @@
-from app import app
-from models import db, User
+from app import create_app
+from extensions import db
 
+app = create_app()
 with app.app_context():
     db.create_all()
-    # 创建初始管理员
-    if not User.query.filter_by(username='admin').first():
-        admin = User(username='admin', role='admin')
-        admin.set_password('123456')
-        db.session.add(admin)
-        db.session.commit()
     print("数据库初始化完成")
